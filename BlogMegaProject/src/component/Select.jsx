b9579ecc058dccca3fed82af6bbeeb5c7c848036
developerSnt <developer.snt@outlook.com>
2024-09-04 12:55:47 -0700

    uploed postcard and login componet

--- BlogMegaProject/src/component/Select.jsx
+++ BlogMegaProject/src/component/Select.jsx
@@ -16,9 +16,9 @@ import React,{useId} from 'react'
         ref={ref}
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}>
         {options?.map((option) =>(
-          <options key={option} value={option}>
+          <option key={option} value={option}>
           {option}
-          </options>
+          </option>
         ))}
       </select>
     </div>
